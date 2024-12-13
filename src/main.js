2181d5ba31ff99c4938e0d0d9fcfca65077aef34
sewwzzzz <1192963064@qq.com>
2024-12-13 21:33:33 +0800

    完成侧边导航栏样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '../src/styles/index.css'
+import '../src/assets/iconfonts/iconfont.css'
 
 createApp(App).mount('#app')
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,18 @@
 <template>
-  <header id="main-header">
+    <header id="main-header">
 
-  </header>
-  <div id="main-div">
-    <aside id="main-aside">
+    </header>
+    <div id="main-div">
+      <aside id="main-aside">
+        <div v-for="(item,index) in menu" :key="index" class="main-title">
+          <div :class="item.icon"></div>
+          <div class="title">{{item.title}}</div>
+        </div>
+      </aside>
+      <article id="main-article">
 
-    </aside>
-    <article id="main-article">
-
-    </article>
-  </div>
+      </article>
+    </div>
 </template>
 
 <style lang="css" scoped>
@@ -21,17 +24,39 @@
 }
 
 #main-div{
-  height:100%;
+  height: calc(100% - 64px - 5px);
   display:flex;
   margin-top:5px;
 }
 
 #main-aside{
-  width:100px;
+  width:150px;
   height:100%;
   background-color:rgb(249, 249, 249);
 }
 
+.main-title{
+  height: 60px;
+  width:100%;
+  /* 位置 */
+  display: flex;
+  align-items:center;
+  /* 字体 */
+  font-weight:400;
+  font-size:14px;
+  color:rgb(81, 92, 107);
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+}
+
+.main-title:hover{
+  background-color: rgb(224, 224, 224);
+  color:rgb(89, 97, 249);
+  cursor:pointer;
+}
+
+.title{
+  margin-left:5px;
+}
 #main-article{
   flex:1;
   height:100%;
@@ -40,5 +65,30 @@
 </style>
 
 <script setup>
-
+const menu = {
+  baseKnowledge: {
+    title: '基础知识',
+    icon: 'iconfont icon-zhishi',
+  },
+  tool: {
+    title: '工具与软件',
+    icon:'iconfont icon-ruanjian',
+  },
+  machineLearning: {
+    title: '机器学习',
+    icon:'iconfont icon-jiqiren',
+  },
+  deepLearning: {
+    title: '深度学习',
+    icon: 'iconfont icon-shenduxuexi',
+  },
+  computerVision: {
+    title: '计算机视觉',
+    icon:'iconfont icon-jisuanjishijue',
+  },
+  naturalLanguageProcessing: {
+    title: '自然语言处理',
+    icon:'iconfont icon-ziranyuyanchuli',
+  }
+}
 </script>
